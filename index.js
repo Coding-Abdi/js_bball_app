b518c9e5082fbaab5fa5824e57f6a13ac2d72fcd
Coding-Abdi <abdi.brum@gmail.com>
2024-08-08 15:05:54 +0100

    added styling

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 let hsEl = document.getElementById("hs-el");
 let gsEl = document.getElementById("gs-el");
-
-// gsEl.textContent = "yes mate"
-// hsEl.textContent = "no mate"
+// let res = document.getElementById("restart-el");
 
 let num1h = 0
 let num2h = 0
@@ -11,38 +9,46 @@ let num1g = 0
 let num2g = 0
 let num3g = 0
 
+let numTotal = 0
+
 function addOneHome() {
     num1h += 1
-    let numTotal = num1h + num2h + num3h
+    numTotal = num1h + num2h + num3h
     hsEl.textContent = numTotal
 }
 
 function addTwoHome() {
     num2h += 2
-    let numTotal = num1h + num2h + num3h
+    numTotal = num1h + num2h + num3h
     hsEl.textContent = numTotal
 }
 
 function addThreeHome() {
     num3h += 3
-    let numTotal = num1h + num2h + num3h
+    numTotal = num1h + num2h + num3h
     hsEl.textContent = numTotal
 }
 
 function addOneGuest() {
     num1g += 1
-    let numTotal = num1g + num2g + num3g
+    numTotal = num1g + num2g + num3g
     gsEl.textContent = numTotal
 }
 
 function addTwoGuest() {
     num2g += 2
-    let numTotal = num1g + num2g + num3g
+    numTotal = num1g + num2g + num3g
     gsEl.textContent = numTotal
 }
 
 function addThreeGuest() {
     num3g += 3
-    let numTotal = num1g + num2g + num3g
+    numTotal = num1g + num2g + num3g
+    gsEl.textContent = numTotal
+}
+
+function restart() {
+    numTotal = 0
+    hsEl.textContent = numTotal
     gsEl.textContent = numTotal
 }
